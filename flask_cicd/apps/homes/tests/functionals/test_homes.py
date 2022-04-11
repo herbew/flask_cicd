4b8586e60db63a97de5563cd8e41ba93f928451e
Herbew <herbew@gmail.com>
2022-04-11 21:59:35 +0700

    test logout

--- flask_cicd/apps/homes/tests/functionals/test_homes.py
+++ flask_cicd/apps/homes/tests/functionals/test_homes.py
@@ -11,13 +11,13 @@ def test_home_route(app, client):
         res = test_client.get('/home')
         assert res.status_code == 308
 
-# def test_logout_route(app, client):
-#     """ 
-#     GIVEN a Flask application configured for testing
-#     WHEN the '/logout' route is requested (GET)
-#     THEN check that the response is valid
-#     """
-#     with app.test_client() as test_client:
-#         res = test_client.get('/logout')
-#         assert res.status_code == 200
-#
+def test_logout_route(app, client):
+    """ 
+    GIVEN a Flask application configured for testing
+    WHEN the '/logout' route is requested (GET)
+    THEN check that the response is valid
+    """
+    with app.test_client() as test_client:
+        res = test_client.get('/logout')
+        assert res.status_code == 200
+
