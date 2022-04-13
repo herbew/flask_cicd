c81668861873b423921c59f9b8c55ec255c08364
Herbew <herbew@gmail.com>
2022-04-13 21:50:32 +0700

    change to Bootstrap

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_sqlalchemy  import SQLAlchemy
-from flask_bootstrap import Bootstrap4
+from flask_bootstrap import Bootstrap, Bootstrap4
 from flask_login import LoginManager
                          
 from flask_cicd.apps.homes.controls import homebp
@@ -13,7 +13,7 @@ from flask_cicd.apps.logins.controls import loginbp
 
 # Set App
 app = Flask(__name__)
-Bootstrap4(app)
+Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databases/db_flask_cicd.db'
 app.config['SECRET_KEY'] = "$HERBEWscKKwwqy#@!"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
