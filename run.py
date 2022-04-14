656bde170a8a1eb9bdcc5e2bc28b69cc24a4db7d
Herbew <herbew@gmail.com>
2022-04-14 23:20:59 +0700

    add tags

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+from sqlalchemy import MetaData
+
 from flask import Flask, render_template, request, flash, redirect, session
 from flask_sqlalchemy  import SQLAlchemy
 from flask_bootstrap import Bootstrap
@@ -16,11 +18,20 @@ app = Flask(__name__, template_folder="templates")
 
 Bootstrap(app)
 
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databases/db_flask_cicd.db'
 app.config['SECRET_KEY'] = "$HERBEWscKKwwqy#@!"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 
-db = SQLAlchemy(app)
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
 
 app.register_blueprint(homebp)
 app.register_blueprint(loginbp)
